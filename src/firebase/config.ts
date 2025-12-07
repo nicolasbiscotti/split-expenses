042a55a41fc06a8d90514f2acfe68e86e395aff3
Nicolas Biscotti <nicolas.biscotti@gmail.com>
2025-12-07 13:08:36 -0300

    export firebase app and configure auth emulator

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
@@ -15,11 +16,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 // Initialize Firestore
 export const db = getFirestore(app);
 
 if (location.hostname === "localhost") {
+  connectAuthEmulator(auth, "http://localhost:9099");
   connectFirestoreEmulator(db, "localhost", 8080);
 }
